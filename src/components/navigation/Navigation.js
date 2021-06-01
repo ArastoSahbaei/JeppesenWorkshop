2ddc0a4f261bac12e8aaf96bb1d608609a599c49
arasto <Arasto.Sahbaei@gmail.com>
2021-06-01 16:38:35 +0200

    on place

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 import { BackDrop } from '../BackDrop'
 import { useLocation } from 'react-router-dom'
 import RoutingPath from '../../routes/RoutingPath'
+import { LaunchProduction } from '../LaunchProduction'
 
 export const Navigation = () => {
 	const location = useLocation()
@@ -12,18 +13,18 @@ export const Navigation = () => {
 
 	const displayViewTitle = () => {
 		switch (location.pathname) {
-			case RoutingPath.dashboardView:
-				return 'Dashboard'
-			case RoutingPath.fileManagementView:
-				return 'File Management'
-			case RoutingPath.landingView:
-				return 'Home'
-			case RoutingPath.hooksView:
-				return 'Hooks'
-			case RoutingPath.RESTView:
-				return 'API'
-			case RoutingPath.propsView:
-				return 'Props'
+		case RoutingPath.dashboardView:
+			return 'Dashboard'
+		case RoutingPath.fileManagementView:
+			return 'File Management'
+		case RoutingPath.landingView:
+			return 'Home'
+		case RoutingPath.hooksView:
+			return 'Hooks'
+		case RoutingPath.RESTView:
+			return 'API'
+		case RoutingPath.propsView:
+			return 'Props'
 		}
 	}
 
@@ -33,6 +34,8 @@ export const Navigation = () => {
 			<Paragraph>{displayViewTitle()}</Paragraph>
 			<SideBar drawerIsOpen={openDrawer} drawerHandler={setOpenDrawer} />
 			{!openDrawer || <BackDrop drawerHandler={setOpenDrawer} />}
+			<LaunchProduction />
+			<Paragraph2>CREW PAIRING</Paragraph2>
 		</Wrapper>
 	)
 }
@@ -41,12 +44,24 @@ const Wrapper = styled.nav`
 	display: grid;
 	grid-template-columns: repeat(20, 1fr);
 	background-color: #263746;
-	padding: 1%;
+	padding: 0.3%;
 `
+
 const Paragraph = styled.p`
 	font-weight: 600;
+	font-size: 1.7rem;
+	cursor: pointer;
+	transition: 0.3s;
+	color: white;
+	grid-column: 2/4;
+
+`
+const Paragraph2 = styled.p`
 	font-size: 1.4rem;
 	cursor: pointer;
 	transition: 0.3s;
 	color: white;
+	grid-column: 7/16;
+	margin: auto;
+	width: 50%;
 `
