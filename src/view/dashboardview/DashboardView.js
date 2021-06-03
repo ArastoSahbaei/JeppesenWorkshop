be66d8dc08f7b1b7f45b0f4ef1fd7f8dd7a277fe
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2021-06-03 09:01:12 +0200

    removed line

--- src/view/dashboardview/DashboardView.js
+++ src/view/dashboardview/DashboardView.js
@@ -3,7 +3,6 @@ export const DashboardView = () => {
 		<div>
 			<h1>TODO: Display charts etc with some data</h1>
 			<h1>Dashboard</h1>
-			<h1>test</h1>
 			<h1>https://react-d3-library.github.io/</h1>
 		</div>
 	)
