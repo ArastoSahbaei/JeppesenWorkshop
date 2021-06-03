411027b62297983125c3a7f530c6f181cd6afeb0
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2021-06-03 16:34:17 +0200

    m

--- src/view/RESTView.js
+++ src/view/RESTView.js
@@ -2,6 +2,8 @@ export const RESTView = () => {
 	return (
 		<div>
 			<h1>API CALLS</h1>
+			<h1>Easiest way to mock API: https://www.mockapi.io/</h1>
+			<h1>Mocking AXIOS: https://www.npmjs.com/package/axios-mock-adapter</h1>
 		</div>
 	)
 }
