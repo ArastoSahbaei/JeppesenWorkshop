fb8f21a65246d09c67f08cec8dae8df22666b87a
arasto <Arasto.Sahbaei@gmail.com>
2021-06-07 09:37:19 +0200

    added functions

--- src/shared/images/api/service/APIService.js
+++ src/shared/images/api/service/APIService.js
@@ -1,9 +1,9 @@
 import http from '../API'
 
 const searchRecipe = (search) => {
-   return http.get(`/search/file?fileName=${search}`)
+	return http.get(`/search/file?fileName=${search}`)
 }
 
 export default {
-   searchRecipe
+	searchRecipe
 }
--- src/view/RESTView.js
+++ src/view/RESTView.js
@@ -1,9 +1,37 @@
+import Axios from 'axios'
+
 export const RESTView = () => {
+
+	const createUser = async () => {
+		try {
+			await Axios.post('https://60ba330980400f00177b7d6a.mockapi.io/api/v1/users', { name: 'Arasto' })
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
+	const getUsers = async () => {
+		try {
+			const { data } = await Axios.get('https://60ba330980400f00177b7d6a.mockapi.io/api/v1/users')
+			console.log(data)
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
 	return (
 		<div>
-			<h1>API CALLS</h1>
-			<h1>Easiest way to mock API: https://www.mockapi.io/</h1>
-			<h1>Mocking AXIOS: https://www.npmjs.com/package/axios-mock-adapter</h1>
+			<h1>1. API CALLS</h1>
+			<h1>2. Structuring API calls</h1>
+			<h1>3. Mocking API calls</h1>
+			<button onClick={() => createUser()}>create</button>
+			<button onClick={() => getUsers()}>get</button>
 		</div>
 	)
 }
+
+
+/*
+	Easiest way to mock API: https://www.mockapi.io/
+	Mocking AXIOS: https://www.npmjs.com/package/axios-mock-adapter
+*/
