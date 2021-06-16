e1dbbba665978f6b2c85f71b7acbe1eeeefc3fd3
arasto <Arasto.Sahbaei@gmail.com>
2021-06-16 20:42:30 +0200

    fixed mock

--- src/tests/api/APIService.test.js
+++ src/tests/api/APIService.test.js
@@ -1,12 +1,13 @@
 import MockAPIService from '../../shared/images/api/service/MockAPIService'
 
 test('get all users', async () => {
-	const { data } = await MockAPIService.getAllUsers()
+	const { data, status } = await MockAPIService.getAllUsers()
 	expect(data).toEqual([
 		{ name: 'arasto' },
 		{ name: 'arasto2' },
 		{ name: 'arasto3' },
 	])
+	expect(status).toBe(200)
 })
 
 
